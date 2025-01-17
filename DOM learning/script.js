e3350a099af7c035f8404c2270e889eb9e803a72
SOUVIK SAHA <souvik21102001@gmail.com>
2025-01-18 02:50:48 +0530

    js updated

--- DOM learning/script.js
+++ DOM learning/script.js
@@ -1,2 +1,11 @@
+//  how to get element and print the value
 let heading = document.getElementById('top-heading').className;
 console.log(heading);
+console.log(document.getElementById('top-heading').getAttribute('class'));
+
+//  how to set any attribute
+let newHeading = document.getElementById('top-heading').setAttribute('class', 'heading1');
+console.log(newHeading);
+
+
+
