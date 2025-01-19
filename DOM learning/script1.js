48482602feda352df85d27884d3c99f01fb65a73
SOUVIK SAHA <souvik21102001@gmail.com>
2025-01-19 20:28:07 +0530

    js updated

--- DOM learning/script1.js
+++ DOM learning/script1.js
@@ -1,3 +1,29 @@
 let week = document.querySelector('.week');
 console.log(week);
 console.log(week.children);
+console.log(week.children[0].innerText);
+//  for loop
+for (let i = 0; i < week.children.length; i++) {
+    console.log(week.children[i].innerText);
+}
+//  firstchild & firstElementChild
+console.log(week.firstChild);
+console.log(week.firstElementChild);
+//  lastchild & lastElementChild
+console.log(week.lastChild);
+console.log(week.lastElementChild);
+
+let dayOne = document.querySelector('.day');
+console.log(dayOne);
+
+console.log(dayOne.parentElement);
+console.log(dayOne.nextElementSibling);
+
+console.log('Nodes: ',week.childNodes);
+
+
+
+
+
+
+
